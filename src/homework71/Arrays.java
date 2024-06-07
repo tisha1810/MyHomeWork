bda440c5df2868081e942491ee1d3c0fb9650885
Tisha1810 <itisha65@gmail.com>
2024-06-07 11:34:32 +0300

    Add - Work with arrays and algorithm

---
+++ src/homework71/Arrays.java
@@ -0,0 +1,61 @@
+package homework71;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = {-22, 3, 41, 56, -15, 2, -55, -9, 97, 11, -10, -8, 5, 20, 10, 4, -71, -19, -48, -25};
+
+        int sum = 0;
+        double result = 0;
+
+        System.out.println("----- First task -----");
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+                sum += numbers[i];
+            }
+        }
+        System.out.println("Sum of negativ numbers is: " + sum);
+        System.out.println("----- Second task ----");
+
+        int counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                counter = counter + 1;
+            }
+        }
+        System.out.println("Number of paired numbers is: " + counter);
+        System.out.println("----- Third task ----");
+
+        counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                counter = counter + 1;
+            }
+        }
+        System.out.println("Number of unpaired numbers is: " + counter);
+        System.out.println("----- Four task ----");
+
+        int minMax = 0;
+        counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < minMax) {
+                minMax = numbers[i];
+                counter = i;
+            }
+        }
+        System.out.println("Minimal number is: " + minMax + " index is " + counter);
+        System.out.println("----- Five task ----");
+
+        minMax = 0;
+        counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > minMax) {
+                minMax = numbers[i];
+                counter = i;
+            }
+        }
+        System.out.println("Maximal number is: " + minMax + " index is " + counter);
+        System.out.println("----- Sixth task ----");
+    }
+}
+
---
+++ src/homework81/Algorithm.java
@@ -0,0 +1,12 @@
+package homework81;
+
+public class Algorithm {
+    public static void main(String[] args) {
+        int[] numbers = {3, 99, 15, 22, 34, 1, 6, 7, 12, 87, 19, 67, 54, 41, 72};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+
+        }
+    }
+}
