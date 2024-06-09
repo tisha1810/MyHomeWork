888b64326ff79d89576339e38a3d16e028412a6f
Tisha1810 <itisha65@gmail.com>
2024-06-09 18:03:55 +0300

    Add - random array in homework 7.1

--- src/homework71/Arrays.java
+++ src/homework71/Arrays.java
@@ -1,12 +1,19 @@
 package homework71;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Arrays {
     public static void main(String[] args) {
-        int[] numbers = {22, 3, 41, 56, -15, 2, -55, -9, 97, 65, -10, -8, 35, 20, -71, 4, 71, -19, 48, -25};
+        int[] numbers = new int[20];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = ThreadLocalRandom.current().nextInt(-100, 100);
+
+        }
+        printArray(numbers);
+
 
         int sum = 0;
-        double result = 0;
-
         System.out.println("----- First task -----");
 
         for (int i = 0; i < numbers.length; i++) {
@@ -14,51 +21,46 @@ public class Arrays {
                 sum += numbers[i];
             }
         }
-        System.out.println("Sum of negativ numbers is: " + sum);
+        System.out.println("Sum of even numbers is: " + sum);
         System.out.println("----- Second task ----");
 
         int counter = 0;
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] % 2 == 0) {
-                counter = counter + 1;
+                counter++;
             }
         }
-        System.out.println("Number of paired numbers is: " + counter);
+        System.out.println("Number of odd numbers is: " + counter);
         System.out.println("----- Third task ----");
 
-        counter = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 != 0) {
-                counter = counter + 1;
-            }
-        }
-        System.out.println("Number of unpaired numbers is: " + counter);
+        sum = numbers.length - counter;
+
+        System.out.println("Number of unpaired numbers is: " + sum);
         System.out.println("----- Four task ----");
 
-        int minOrMax = 0;
+        int min = 0;
         counter = 0;
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < minOrMax) {
-                minOrMax = numbers[i];
+            if (numbers[i] < min) {
+                min = numbers[i];
                 counter = i;
             }
         }
-        System.out.println("Minimal number is: " + minOrMax + " (index is " + counter + ")");
+        System.out.println("Minimal number is: " + min + " (index is " + counter + ")");
         System.out.println("----- Five task ----");
 
-        minOrMax = 0;
+        int max = 0;
         counter = 0;
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > minOrMax) {
-                minOrMax = numbers[i];
+            if (numbers[i] > max) {
+                max = numbers[i];
                 counter = i;
             }
         }
-        System.out.println("Maximal number is: " + minOrMax + " (index is " + counter + ")");
+        System.out.println("Maximal number is: " + max + " (index is " + counter + ")");
         System.out.println("----- Sixth task ----");
 
         sum = 0;
-        result = 0;
         counter = 0;
         for (int i = 0; i < numbers.length; i++) {
             counter = i;
@@ -69,9 +71,22 @@ public class Arrays {
         for (int i = counter + 1; i < numbers.length; i++) {
             sum += numbers[i];
         }
+        double result = 0;
+
         result = (double) sum / (numbers.length - (counter + 1));
         System.out.printf("Avarage sum is %.2f \n", result);
         System.out.println("----- The End -----");
     }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+        }
+        System.out.println();
+
+
+    }
 }
+
 
