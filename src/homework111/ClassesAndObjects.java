5494b85630713b9c5af0bc5ae3e9ae613a4a87f7
Tisha1810 <itisha65@gmail.com>
2024-06-21 16:15:21 +0300

    Add - change profession method

--- src/homework111/ClassesAndObjects.java
+++ src/homework111/ClassesAndObjects.java
@@ -1,10 +1,19 @@
 package homework111;
 
+import java.util.Scanner;
+
 public class ClassesAndObjects {
     public static void main(String[] args) {
+        System.out.println("---- 1 st ----");
+
         Person firstPerson = new Person("John", 45, "doctor");
-        Person secondPerson = new Person("Anna", 37, "journalist");
-        Person thirdPerson = new Person("Matt", 55, "designer");
+        Person secondPerson = new Person("Anna", 37, "designer");
+        Person thirdPerson = new Person("Matt", 55, "policeman");
+
+        System.out.println("---- 2 nd ----");
+
+        Person fourPerson = new Person("Tom", 65, "actor");
+        Person fivePer = new Person("Tom",65);
 
 
     }
@@ -14,12 +23,28 @@ class Person {
     String name;
     int age;
     String profession;
+    String newProfession;
 
     public Person(String name, int age, String profession) {
         this.name = name;
         this.age = age;
         this.profession = profession;
         System.out.println("Name: " + name + ", age " + age + ", work: " + profession);
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("Name: " + name + ", age " + age + ", work: " + changeProfession(profession));
+
+    }
+
+
+    public static String changeProfession(String profession) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter new profession:");
+        profession = scanner.nextLine();
+        return profession;
     }
 }
 
