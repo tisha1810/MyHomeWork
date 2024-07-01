b11d776e7902c81e4020d9427a1a4c58d88cc505
Tisha1810 <itisha65@gmail.com>
2024-07-01 22:24:33 +0300

    Change access modifier

--- src/homework131/Triangle.java
+++ src/homework131/Triangle.java
@@ -1,8 +1,8 @@
 package homework131;
 
 public class Triangle implements Calculable {
-    private int base;
-    private int highest;
+    int base;
+    int highest;
 
     public Triangle(int base, int highest) {
         this.base = base;
