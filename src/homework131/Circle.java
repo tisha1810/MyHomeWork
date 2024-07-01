03782de1b69003f288c536e79bb946ed60701521
Tisha1810 <itisha65@gmail.com>
2024-07-01 22:17:19 +0300

    Create class "circle"

---
+++ src/homework131/Circle.java
@@ -0,0 +1,11 @@
+package homework131;
+
+public class Circle implements Calculable {
+    public Circle(){
+
+    }
+    @Override
+    public double calculateSquare() {
+        return 0;
+    }
+}
