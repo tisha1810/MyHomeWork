f05267c973349bbd2ee5eb9455b74b1914a7e24f
Tisha1810 <itisha65@gmail.com>
2024-07-02 08:56:18 +0300

    Rename geometric figures to figures

---
+++ src/homework131/Figures.java
@@ -0,0 +1,29 @@
+package homework131;
+
+public class Figures implements Calculable {
+    public static void main(String[] args) {
+        Figures[] figures = {
+                new Circle(Math.PI, 2),
+                new Square(4, 4),
+                new Triangle(4, 7)
+        };
+        System.out.printf("%.2f", sumAllSquares(figures));
+    }
+
+
+    @Override
+    public double calculate() {
+        return 0;
+    }
+
+    public static double sumAllSquares(Figures[] figures) {
+        double sum = 0;
+        for (Figures figure : figures) {
+            sum += figure.calculate();
+
+        }
+        return sum;
+
+    }
+
+}
