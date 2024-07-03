21ff8255df31a4a771df94132aa3c2c1e0e00256
Tisha1810 <itisha65@gmail.com>
2024-07-03 23:56:17 +0300

    Add array of figures

--- src/homework131/FigureRunner.java
+++ src/homework131/FigureRunner.java
@@ -1,4 +1,12 @@
 package homework131;
 
 public class FigureRunner {
+    public static void main(String[] args) {
+        CalculableFigure[] figures = {
+                new Circle(7),
+                new Triangle(9,11),
+                new Square(8)
+        };
+
+    }
 }
