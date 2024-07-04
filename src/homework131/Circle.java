144b984f38ee463f7fcf017bf4c74d3a2767b316
Tisha1810 <itisha65@gmail.com>
2024-07-04 12:30:50 +0300

    Delete area field, and area variable

--- src/homework131/Circle.java
+++ src/homework131/Circle.java
@@ -1,18 +1,14 @@
 package homework131;
 
 public class Circle implements CalculableFigure {
-    private final double PI;
     private int radius;
-    private double area;
 
     public Circle(int radius) {
         this.radius = radius;
-        PI = Math.PI;
-        area = calculateArea();
     }
 
     @Override
     public double calculateArea() {
-        return PI * (radius * radius);
+        return Math.PI * (radius * radius);
     }
 }
