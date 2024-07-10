9401116262f7958812a27cddba61aa5e8d15f5a4
Tisha1810 <itisha65@gmail.com>
2024-07-10 10:34:18 +0300

    delete empty line

--- src/homework151/DayDemo.java
+++ src/homework151/DayDemo.java
@@ -1,9 +1,8 @@
 package homework151;
 
 public class DayDemo {
-
     public static void main(String[] args) {
-     Day day = Day.SATURDAY;
-     day.advice(day);
+        Day day = Day.SATURDAY;
+        day.advice(day);
     }
 }
