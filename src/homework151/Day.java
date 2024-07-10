fb8fa397e76b6a3f740b8ed18b60801347779946
Tisha1810 <itisha65@gmail.com>
2024-07-10 11:39:44 +0300

    Delete method advice

--- src/homework151/Day.java
+++ src/homework151/Day.java
@@ -1,6 +1,6 @@
 package homework151;
 
-public enum Day implements DayAdvice {
+public enum Day{
     MONDAY,
     TUESDAY,
     WEDNESDAY,
@@ -8,31 +8,4 @@ public enum Day implements DayAdvice {
     FRIDAY,
     SATURDAY,
     SUNDAY;
-
-    @Override
-    public void advice(Day day) {
-        switch (day) {
-            case MONDAY:
-                System.out.println("To be productive in job!");
-                break;
-            case TUESDAY:
-                System.out.println("To be productive in job!");
-                break;
-            case WEDNESDAY:
-                System.out.println("To be productive in job!");
-                break;
-            case THURSDAY:
-                System.out.println("To be productive in job!");
-                break;
-            case FRIDAY:
-                System.out.println("Finally FRIDAY, yooouuu hooooou!");
-                break;
-            case SATURDAY:
-                System.out.println("Visit a restaurant!");
-                break;
-            case SUNDAY:
-                System.out.println("Visit a cinema!");
-                break;
-        }
-    }
 }
