78418ee8b8f8a198108ea11377bfd1a44ab88efc
Tisha1810 <itisha65@gmail.com>
2024-07-14 14:20:33 +0300

    Add lines .... Test

--- src/test/java/homework161/CalculatorTest.java
+++ src/test/java/homework161/CalculatorTest.java
@@ -16,24 +16,28 @@ class CalculatorTest {
 
     @Test
     void additionTest() {
+        System.out.println("Addition Test");
         double result = calculator.addition(5, 5);
         Assertions.assertEquals(10, result);
     }
 
     @Test
     void subtractionTest() {
+        System.out.println("Subtraction Test");
         double result = calculator.subtraction(5, 5);
         Assertions.assertEquals(0, result);
     }
 
     @Test
     void multiplyTest() {
+        System.out.println("Multiplication Test");
         double result = calculator.multiplication(5, 2);
         Assertions.assertEquals(10, result);
     }
 
     @Test
     void dividingTest() {
+        System.out.println("Dividing Test");
         double result = calculator.dividing(2, 2);
         Assertions.assertEquals(1, result);
     }
