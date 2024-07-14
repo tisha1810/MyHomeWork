7c371d71df28a9b72d4620494fcb504e45fbae8d
Tisha1810 <itisha65@gmail.com>
2024-07-14 14:16:00 +0300

    add class calculator

---
+++ src/main/java/homework161/Calculator.java
@@ -0,0 +1,20 @@
+package homework161;
+
+public class Calculator {
+
+    public double addition(double a, double b) {
+        return a + b;
+    }
+
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public double dividing(double a, double b) {
+        return a / b;
+    }
+}
