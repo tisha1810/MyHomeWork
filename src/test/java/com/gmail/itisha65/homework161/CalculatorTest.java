c5e756c25b13cff162a2b141dbba5e3d22285bd6
Tisha1810 <itisha65@gmail.com>
2024-07-15 11:03:23 +0300

    Refactor and edit calculator test

--- src/test/java/com/gmail/itisha65/homework161/CalculatorTest.java
+++ src/test/java/com/gmail/itisha65/homework161/CalculatorTest.java
@@ -1,4 +1,4 @@
-package homework161;
+package com.gmail.itisha65.homework161;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,34 +10,28 @@ class CalculatorTest {
     @BeforeEach
     void setUp() {
         this.calculator = new Calculator();
-        System.out.println("New calculator: " + calculator.hashCode());
-
     }
 
     @Test
     void additionTest() {
-        System.out.println("Addition Test");
         double result = calculator.addition(5, 5);
         Assertions.assertEquals(10, result);
     }
 
     @Test
     void subtractionTest() {
-        System.out.println("Subtraction Test");
         double result = calculator.subtraction(5, 5);
         Assertions.assertEquals(0, result);
     }
 
     @Test
     void multiplyTest() {
-        System.out.println("Multiplication Test");
         double result = calculator.multiplication(5, 2);
         Assertions.assertEquals(10, result);
     }
 
     @Test
     void dividingTest() {
-        System.out.println("Dividing Test");
         double result = calculator.dividing(2, 2);
         Assertions.assertEquals(1, result);
     }
