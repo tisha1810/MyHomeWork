093b9a820d108c0c9e98e9de336ab0713e9e7c2d
Tisha1810 <81794273+tisha1810@users.noreply.github.com>
2024-07-19 20:30:59 +0300

    Add files via upload

---
+++ .github/workflows/CalculatorTest.java
@@ -0,0 +1,52 @@
+package com.gmail.itisha65.homework161;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        this.calculator = new Calculator();
+    }
+
+    @Test
+    void additionTest() {
+        double result = calculator.addition(5, 5);
+
+        assertEquals(10, result);
+    }
+
+    @Test
+    void subtractionTest() {
+        double result = calculator.subtraction(5, 5);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void multiplyTest() {
+        double result = calculator.multiplication(5, 2);
+
+        assertEquals(10, result);
+    }
+
+    @Test
+    void dividingTest() {
+        double result = calculator.dividing(2, 2);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void dividingZeroTest() {
+        ArithmeticException exception = assertThrows(ArithmeticException.class, () -> {
+            calculator.dividing(10, 0);
+        });
+        assertEquals("Invalid operation", exception.getMessage());
+    }
+}
